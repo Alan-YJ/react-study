7971d94969b5e583d58f932b98e4c828b2a042e0
yijing <734732794@qq.com>
2020-03-09 11:45:45 +0800

    base-p6

---
+++ project/src/AppFunction.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div className="list">
+            <ul>
+                <li>study</li>
+                <li>react</li>
+                <li>.</li>
+                <li>js</li>
+            </ul>
+        </div>
+    )
+}
+
+export default App;
