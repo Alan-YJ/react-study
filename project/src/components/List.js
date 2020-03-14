5695bff815867c3a4bd7a1159482fca945cd3144
yijing <734732794@qq.com>
2020-03-14 21:21:10 +0800

    base-p27

--- project/src/components/List.js
+++ project/src/components/List.js
@@ -1,41 +1,66 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { CSSTransition,TransitionGroup } from 'react-transition-group'
 
-class List extends Component{
-    constructor(props){
+class List extends Component {
+    constructor(props) {
         super(props)
+        this.state = {
+            willRender: false
+        }
     }
-    render(){
-        return(
+
+    componentWillMount() {
+        setTimeout(() => {
+            this.setState({
+                willRender: true
+            })
+        }, 1000)
+    }
+
+    render() {
+        let willRender = this.state.willRender
+        return (
             <ul className="list">
-                {
-                    this.props.list.map((item,index)=>{
-                        return(
-                            <li className="item" key={item.id}>
-                                <span>{item.name}</span>
-                                <span>
-                                    <button onClick={this.editItem.bind(this,item)}>编辑</button>
-                                    <button onClick={this.delItem.bind(this,item,index)}>删除</button>
-                                </span>
-                            </li>
-                        )
-                    })
-                }
+                <TransitionGroup>
+                    {
+                        this.props.list.map((item, index) => {
+                            return (
+                                <CSSTransition
+                                    timeout={index*50}
+                                    classNames='list-item'
+                                    in={this.state.willRender}
+                                    key={item.id}
+                                    appear={true}
+                                    unmountOnExit
+                                >
+                                    <li className="item">
+                                        <span>{item.name}</span>
+                                        <span>
+                                            <button onClick={this.editItem.bind(this, item)}>编辑</button>
+                                            <button onClick={this.delItem.bind(this, item, index)}>删除</button>
+                                        </span>
+                                    </li>
+                                </CSSTransition>
+                            )
+                        })
+                    }
+                </TransitionGroup>
             </ul>
         )
     }
-    editItem(item){
+    editItem(item) {
         this.props.editItem(item)
     }
-    delItem(item,index){
-        this.props.delItem(item,index)
+    delItem(item, index) {
+        this.props.delItem(item, index)
     }
 }
 
 List.propTypes = {
-    list:PropTypes.array,
-    editItem:PropTypes.func,
-    delItem:PropTypes.func
+    list: PropTypes.array,
+    editItem: PropTypes.func,
+    delItem: PropTypes.func
 }
 
 export default List
