cbb623ebf1ff444c26d3d87e13fa4fb45567ef5d
yijing <734732794@qq.com>
2020-03-31 21:26:57 +0800

    redux-p23

--- project/src/App.js
+++ project/src/App.js
@@ -1,19 +1,13 @@
 import React, { Component, Fragment } from 'react'
-// import Header from './components/Header'
-import List from './components/List'
-import './App.css'
-// import store from './store/index'
-
 
 class App extends Component {
-  constructor(props){
-    super(props)
-  }
+  state = {  }
   render() { 
     return ( 
       <Fragment>
-        {/* <Header></Header> */}
-        <List></List>
+        <div>
+          test
+        </div>
       </Fragment>
      );
   }
--- project/src/index.js
+++ project/src/index.js
@@ -1,16 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import 'antd/dist/antd.css'
+import react from 'react'
+import dom from 'react-dom'
+import App from './App'
 import './App.css'
-import { Provider } from "react-redux";
-import store from "./store";
+import  'antd/dist/antd.css'
 
-import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  rootElement
-);
+
+dom.render(App,document.getElementById('root'))
