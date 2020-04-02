d81b6d4625de8d0281270f32d17b8cd83528330e
yijing <734732794@qq.com>
2020-04-02 11:25:20 +0800

    router-p2

---
+++ project/src/Pages/Index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Index extends Component {
+    constructor(props){
+        super(props)
+    }
+    render() { 
+        return ( 
+            <div>
+                <h2>test</h2>
+            </div>
+         );
+    }
+}
+ 
+export default Index;
