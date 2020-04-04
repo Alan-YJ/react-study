14a2c0a9f8b612914a9492e3b6442256cbc954a5
yijing <734732794@qq.com>
2020-04-04 17:20:31 +0800

    hooks-p4

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,15 +1,45 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-function Page(){
-    const [count,setCount] = useState(0)
+function Index(){
+    let [count,setCount] = useState(0)
     useEffect(()=>{
-        console.info(`click count:${count}`)
-    })
+        console.info('index Page')
+        return ()=>{
+            console.info('unmount index page')
+        }
+    },[])
     return(
         <div>
-            <p>you click count {count}</p>
+            <h2>Index Page</h2>
+            <p>you click count:{count}</p>
             <button onClick={()=>{setCount(count+1)}}>click</button>
         </div>
+    )
+}
+
+function List(){
+    return(
+        <div>
+            <h2>List Page</h2>
+        </div>
+    )
+}
+
+function Page(){
+    return(
+        <Router>
+            <ul>
+                <li>
+                    <Link to='/'>Index</Link>
+                </li>
+                <li>
+                    <Link to='/list'>List</Link>
+                </li>
+            </ul>
+            <Route path='/' exact component={Index}></Route>
+            <Route path='/list' component={List}></Route>
+        </Router>
     )
 }
 
