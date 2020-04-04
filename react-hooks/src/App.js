0b71ddf47b1685fe155130d44b97d1bfbf1e9d82
yijing <734732794@qq.com>
2020-04-04 16:47:33 +0800

    hooks-p3

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Page(){
     const [count,setCount] = useState(0)
+    useEffect(()=>{
+        console.info(`click count:${count}`)
+    })
     return(
         <div>
             <p>you click count {count}</p>
