d4a6fceb43b037a77f24fd8dc6956539a06e9f2b
yijing <734732794@qq.com>
2020-04-04 09:34:19 +0800

    router-p9

--- project/src/AppRouter.js
+++ project/src/AppRouter.js
@@ -6,21 +6,37 @@ import WorkPlace from './Pages/WorkPlace'
 import './index.css'
 import 'antd/dist/antd.css'
 
+const routeConfig = [
+    { path:'/',title:'首页',exact:true,component:Index},
+    { path:'/video',title:'视频教程',exact:false,component:Video},
+    { path:'/workplace',title:'职场技能',exact:false,component:WorkPlace},
+]
+
 class AppRouter extends Component {
     render() { 
         return ( 
             <Router>
                 <div className='header'>
                     <ul className='nav-list'>
-                        <li><Link to='/'>首页</Link> </li>
-                        <li><Link to='/video'>视频教程</Link></li>
-                        <li><Link to='/workplace'>职场技能</Link></li>
+                        {
+                            routeConfig.map(linkItem=>{
+                                return(
+                                    <li>
+                                        <Link to={linkItem.path}>{linkItem.title}</Link>
+                                    </li>
+                                )
+                            })
+                        }
                     </ul>
                 </div>
                 <div className='content'>
-                    <Route path='/' exact component={Index}></Route>
-                    <Route path='/video' component={Video}></Route>
-                    <Route path='/workplace' component={WorkPlace}></Route>
+                    {
+                        routeConfig.map(linkItem=>{
+                            return(
+                                <Route path={linkItem.path} exact={linkItem.exact} component={linkItem.component}></Route>
+                            )
+                        })
+                    }
                 </div>
             </Router>
          );
