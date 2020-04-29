253622443b8ce829d89622a167299e67286a6820
yijing <yijing5893@dingtalk.com>
2020-04-29 18:15:26 +0800

    最佳实践

---
+++ bestpractice/src/components/LogoutButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LogoutButton = ()=>{
+    return (
+        <button>退出</button>
+    )
+}
+
+export default LogoutButton
---
+++ bestpractice/test.js
@@ -0,0 +1,5 @@
+var user = { firstName: 'yi', lastName: 'jing' };
+var greeter = function (user) {
+    return "Hello! " + user.firstName + "." + user.lastName;
+};
+greeter(user);
