85ed6dee4bc88599d6c4a2fa6cdc869441908a26
老妖怪 <oncwnuKMqAhaF_4PRTR7LJ0kr0OU@git.weixin.qq.com>
2020-05-06 07:44:27 +0800

    menu组件基础需求分析

---
+++ rt-ui/src/components/Menu/menu.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Menu:React.FC = ()=>{
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Menu
+
+
+/*
+    1.横向/纵向滚动条
+    2.高亮
+    3.disabled
+    4.下拉菜单/展开菜单
+
+*/
